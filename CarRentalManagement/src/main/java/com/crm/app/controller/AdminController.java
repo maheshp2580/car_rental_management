f6cfe39d52972fb0a5cfb1f8044b498e78e7ece9
maheshp2580 <maheshp250900@gmail.com>
2024-02-28 17:49:55 -0600

    search and filter cars

---
+++ CarRentalManagement/src/main/java/com/crm/app/controller/AdminController.java
@@ -0,0 +1,54 @@
+package com.crm.app.controller;
+
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.crm.app.model.Car;
+import com.crm.app.model.User;
+import com.crm.app.service.AdminService;
+
+
+@Controller
+public class AdminController {
+	
+	@Autowired
+	private AdminService adminService;
+	
+	
+
+	@GetMapping("/admin")
+	public String getAdminWelcomePage(@ModelAttribute("user") User user, Model model, HttpSession session)
+	{
+		@SuppressWarnings("unchecked")
+        List<String> messages = (List<String>) session.getAttribute("MY_SESSION_MESSAGES");
+
+		if(messages == null) {
+			model.addAttribute("errormsg", "Session Expired. Please Login Again");
+			return "home/error";
+		}
+        model.addAttribute("sessionMessages", messages);
+
+		return "admin/adminwelcome";
+	}
+	
+	
+
+		
--- CarRentalManagement/src/main/java/com/crm/app/service/UserServiceImpl.java
+++ CarRentalManagement/src/main/java/com/crm/app/service/UserServiceImpl.java
@@ -12,21 +12,9 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.crm.app.dao.BookCarRepo;
-import com.crm.app.dao.BookDriverRepo;
 import com.crm.app.dao.CarRepo;
-import com.crm.app.dao.DriverRepo;
-import com.crm.app.dao.FeedbackRepo;
-import com.crm.app.dao.PaymentRepo;
-import com.crm.app.dao.RatingRepo;
 import com.crm.app.dao.UserRepo;
-import com.crm.app.model.BookCar;
-import com.crm.app.model.BookDriver;
 import com.crm.app.model.Car;
-import com.crm.app.model.Driver;
-import com.crm.app.model.Feedback;
-import com.crm.app.model.Payment;
-import com.crm.app.model.Rating;
 import com.crm.app.model.User;
 
 
@@ -39,103 +27,122 @@ public class UserServiceImpl implements UserService{
 	
 	@Autowired
 	private CarRepo carRepo;
-	
-	@Autowired
-	private BookCarRepo bookCarRepo;
-	
-	@Autowired
-	private BookDriverRepo bookDriverRepo;
-	
-	@Autowired
-	private PaymentRepo paymentRepo;
-	
-	@Autowired
-	private DriverRepo driverRepo;
-	
-	@Autowired
-	private RatingRepo ratingRepo;
-	@Autowired
-	private FeedbackRepo feedbackRepo;
 
 	
-	
-
-	@Override
-	public List<BookCar> getUserCarBookings(String email) {
-		// TODO Auto-generated method stub
-		return bookCarRepo.findAll().stream().filter(bc -> bc.getUserEmail().equals(email)).collect(Collectors.toList());
+	public int saveUser(User user) {
+		user.setUsertype("customer");
+		userRepo.save(user);
 		
-	}
-
-	@Override
-	public List<Driver> filterDrivers(String experience, String rating, String price) {
-		if(experience.isEmpty() && rating.isEmpty() && price.isEmpty()) {
-			return  driverRepo.findAll();
-		}
-		
-		
-		List<Driver> drivers = driverRepo.findAll();
-		List<Driver> filteredDrivers = new ArrayList<Driver>();
-		
-		if(!experience.isEmpty() && rating.isEmpty() && price.isEmpty()) {
-			filteredDrivers = drivers.stream().filter(driver -> Integer.parseInt(driver.getNoOfYearsExperience()) >= Integer.parseInt(experience)).collect(Collectors.toList());	
-		}
-		
-		else if(experience.isEmpty() && !rating.isEmpty() && price.isEmpty()) {
-			filteredDrivers = drivers.stream().filter(driver -> Integer.parseInt(driver.getRating()) >= Integer.parseInt(rating) ).collect(Collectors.toList());	
-		}
-		
-		else if(experience.isEmpty() && rating.isEmpty() && !price.isEmpty()) {
-			filteredDrivers = drivers.stream().filter(driver -> Integer.parseInt(driver.getPricePerDay()) <= Integer.parseInt(price)).collect(Collectors.toList());	
-		}
-		
-		else if(!experience.isEmpty() && !rating.isEmpty() && price.isEmpty()) {
-			filteredDrivers = drivers.stream().filter(driver -> Integer.parseInt(driver.getNoOfYearsExperience()) >= Integer.parseInt(experience) && Integer.parseInt(driver.getRating()) >= Integer.parseInt(rating)).collect(Collectors.toList());	
-		}
-		
-		else if(experience.isEmpty() && !rating.isEmpty() && !price.isEmpty()) {
-			filteredDrivers = drivers.stream().filter(driver -> Integer.parseInt(driver.getRating()) >= Integer.parseInt(rating) && Integer.parseInt(driver.getPricePerDay()) <= Integer.parseInt(price)).collect(Collectors.toList());	
-		}
-		
-		else if(!experience.isEmpty() && rating.isEmpty() && !price.isEmpty()) {
-			filteredDrivers = drivers.stream().filter(driver -> Integer.parseInt(driver.getNoOfYearsExperience()) >= Integer.parseInt(experience) && Integer.parseInt(driver.getPricePerDay()) <= Integer.parseInt(price)).collect(Collectors.toList());	
+		if(userRepo.save(user)!=null) {
+			return 1;
 		}
 		else {
-			filteredDrivers = drivers.stream().filter(driver -> Integer.parseInt(driver.getNoOfYearsExperience()) >= Integer.parseInt(experience) && Integer.parseInt(driver.getRating()) >= Integer.parseInt(rating) && Integer.parseInt(driver.getPricePerDay()) <= Integer.parseInt(price)).collect(Collectors.toList());	
+			return 0;
+		}
+	}
+	
+	public User findUser(String email) {
+		List<User> user = userRepo.findAll();
+		System.out.println("----"+user.size());
+		if(user.size() == 0) {
+			return null;
+		}
+		List<User> veifiedUser = user.stream().filter(n -> n.getEmail().equals(email) || n.getUsername().equals(email)).collect(Collectors.toList());
+		if(veifiedUser.size() > 0) {
+			return veifiedUser.get(0);
+		}
+		else {
+			return null;
 		}
 		
-		 
+	}
+	
+	public User authenticateUser(User user) {
+		
+		if(user.getEmail().equals("admin@gmail.com") && user.getPassword().equals("admin")) {
+			
+			user.setUsertype("admin");
+			
+			return user;
+		}
+		
+		List<User> users = userRepo.findAll();
+		List<User> veifiedUser = users.stream().filter(n -> (n.getEmail().equals(user.getEmail()) || n.getUsername().equals(user.getEmail())) && n.getPassword().equals(user.getPassword())).collect(Collectors.toList());
+		
+		if(veifiedUser.size() ==1) {
+			
+			
+			return veifiedUser.get(0);
+		}
+		else {
+				return null;
+			
+		}
+			
+	}
+	
+	public User findUserByUsername(String username) {
+		
+		List<User> users = userRepo.findAll();
+		List<User> veifiedUser = users.stream().filter(n -> n.getUsername().equals(username)).collect(Collectors.toList());
+		if(veifiedUser.size() > 0) {
+			return veifiedUser.get(0);
+		}
+		else {
+			return null;
+		}
+		
+	}
+	
+	public int validatePassword(User usermodel, String securityQuestion, String securityAnswer) {
 		
 		
 		
-		return filteredDrivers.stream().collect(collectingAndThen(toCollection(() -> new TreeSet<>(comparingLong(Driver::getId))),
-                ArrayList::new));
+		List<User> users = userRepo.findAll();
+		List<User> verifiedUser = users.stream().filter(n -> n.getEmail().equals(usermodel.getEmail())).collect(Collectors.toList());
+		if(verifiedUser.size() ==1) {
+			List<User> userSecurities = userRepo.findAll();
+			
+			List<User> securedUser = userSecurities.stream().filter(security -> security.getSecurityQuestion().equals(securityQuestion) && security.getSecurityAnswer().equals(securityAnswer)
+					
+					).collect(Collectors.toList());
+			if(securedUser.size() == 1) {
+				return 1;
+			}
+			else {
+				return 2;
+			}
+		}
+		else {
+			return 0;
+		}
+		
+		
+	}
+	
+	public void saveNewPassword(User usermodel) {
+		
+		
+		
+			User user = userRepo.findbyEmail(usermodel.getEmail());
+			System.out.println("user#########"+user.toString());
+			user.setPassword(usermodel.getPassword());
+			userRepo.save(user);
+			
+		
+	}
+	
+	public void deleteUser(Long id) {
+			
+			userRepo.deleteById(id);
+			
 	}
 
-	@Override
-	public Driver getDriverById(Long id) {
-		// TODO Auto-generated method stub
-		return driverRepo.getById(id);
-		
-	}
 
 	@Override
-	public void saveDriverBooking(BookDriver bookdriver) {
+	public List<Car> getAllCar() {
 		// TODO Auto-generated method stub
-		bookDriverRepo.save(bookdriver);
-		
-	}
-
-	@Override
-	public BookDriver getUserDirverBooking(String email) {
-		// TODO Auto-generated method stub
-		return bookDriverRepo.findAll().stream().filter(bc -> bc.getUserEmail().equals(email) && bc.getStatus().equals("payment_pending")).collect(Collectors.toList()).get(0);
-	}
-
-	@Override
-	public List<BookDriver> getUserDriverBookings(String email) {
-		// TODO Auto-generated method stub
-		return bookDriverRepo.findAll().stream().filter(bc -> bc.getUserEmail().equals(email)).collect(Collectors.toList());
+		return carRepo.findAll();
 	}
 
 }
